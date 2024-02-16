c16c03fc409a503a65bc52ed23611b1125e8ae1c
Ricardo Gomes <mail@ricardogomes.me>
2024-02-16 22:53:24 +0000

    feat: implement login page
    implement login page
    implement login form
    style login form
    
    closes #3

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="mx-auto">
+      <h1 className="text-2xl text-center mt-20<">Login Page</h1>
+      <LoginForm />
+    </div>
+  );
+}
