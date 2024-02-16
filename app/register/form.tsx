95e0d86773ebd64b2b5124bf2b5774522650fc8a
Ricardo Gomes <mail@ricardogomes.me>
2024-02-16 15:26:54 +0000

    chore: base register page structure
    add register page
    add register form
    
    working on #4

---
+++ app/register/form.tsx
@@ -0,0 +1,9 @@
+export default function RegisterForm() {
+  return (
+    <form>
+      <input type="email" />
+      <input type="password" />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
