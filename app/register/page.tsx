8ce2092f485b285c56638f950d2e55c885134f77
Ricardo Gomes <mail@ricardogomes.me>
2024-02-16 22:38:22 +0000

    Register page (#13)
    * chore: base register page structure
    
    add register page
    add register form
    
    working on #4
    
    * chore: add shadcn/ui
    
    add shadcn/ui
    add button and input component
    
    * feat: implement register page
    
    implement register page
    implement register form
    style register form
    
    closes #4

---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from "./register-form";
+
+export default function RegisterPage() {
+  return (
+    <div className="mx-auto">
+      <h1 className="text-2xl text-center mt-20<">Register Page</h1>
+      <RegisterForm />
+    </div>
+  );
+}
