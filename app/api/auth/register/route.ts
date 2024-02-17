b6af3870a1e8c508a8fd7dd056c3c10d335dc98c
Ricardo Gomes <mail@ricardogomes.me>
2024-02-17 18:19:51 +0000

    User registration (#18)
    * chore: add dependencies
    
    add shadcn toaster
    add @types/bcrypt
    
    * feat: implement user registration
    
    implement user register persistence
    implement register form api call
    
    closes #7

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+import { hash } from "bcrypt";
 
 export async function POST(request: Request) {
   try {
     const { email, password } = await request.json();
-    console.log("register", { email });
 
-    //TODO: Implement database logic here
+    //TODO: Add validation for email and password
 
+    const hashedPassword = await hash(password, 10);
+
+    const dbResult = await sql`
+      INSERT INTO users (email, password)
+      VALUES (${email}, ${hashedPassword})
+    `;
+    if (!dbResult.rowCount || dbResult.rowCount === 0) {
+      return NextResponse.json(
+        { message: "User not registered to database" },
+        { status: 500 }
+      );
+    }
     return NextResponse.json({ message: "User registered successfuly" });
   } catch (error) {
     console.log("error", error);
--- app/register/register-form.tsx
+++ app/register/register-form.tsx
@@ -1,11 +1,54 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function RegisterForm() {
+  const { toast } = useToast();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    try {
+      const response = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        toast({
+          description: "User registered successfuly",
+        });
+      } else {
+        toast({
+          variant: "destructive",
+          description: "User not registered",
+        });
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description:
+          "An error occoured while registering the user. Please try again.",
+      });
+      console.log("error", error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-2 mx-auto max-w-md mt-10">
-      <Input type="email" placeholder="Your email address" />
-      <Input type="password" placeholder="Your password" />
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 mx-auto max-w-md mt-10"
+    >
+      <Input type="email" name="email" placeholder="Your email address" />
+      <Input type="password" name="password" placeholder="Your password" />
       <Button type="submit" variant="secondary">
         Register
       </Button>
