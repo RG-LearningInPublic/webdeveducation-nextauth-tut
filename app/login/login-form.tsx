3fb7d97c85b49a99a01e2d6470d83abe8158e231
Ricardo Gomes <mail@ricardogomes.me>
2024-02-17 23:34:09 +0000

    feat: User Authentication (#19)
    Implement next auth credencials authorize handler
    Implement login form
    
    Had to change .env to place VERCEL_URL="http://localhost:3000/" to fix
    an node Invalid URL error
    
    closes #16

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -1,13 +1,49 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const response = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (!response?.error) {
+      router.push("/");
+      router.refresh();
+    } else {
+      toast({
+        variant: "destructive",
+        description:
+          "An error occoured while logging in the user. Please try again.",
+      });
+      console.log("error", response.error);
+    }
+  };
   return (
-    <form className="flex flex-col gap-2 mx-auto max-w-md mt-10">
-      <Input type="email" placeholder="Your email address" />
-      <Input type="password" placeholder="Your password" />
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 mx-auto max-w-md mt-10"
+    >
+      <Input type="email" name="email" placeholder="Your email address" />
+      <Input type="password" name="password" placeholder="Your password" />
       <Button type="submit" variant="secondary">
-        Login
+        Register
       </Button>
     </form>
   );
