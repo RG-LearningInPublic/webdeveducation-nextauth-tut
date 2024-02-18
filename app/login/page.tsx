f7dafb8539532339dce5b59556e6398f8bbfaad9
Ricardo Gomes <mail@ricardogomes.me>
2024-02-18 00:13:28 +0000

    feat: User Authorization (#22)
    Implement session verification
    Implement dashboard page
    Implement middleware - make dashboard private
    Guard login and register pages against logged in users
    
    closes #8

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,12 @@
+import { getServerSession } from "next-auth";
 import LoginForm from "./login-form";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession();
+  if (session) {
+    redirect("/");
+  }
   return (
     <div className="mx-auto">
       <h1 className="text-2xl text-center mt-20<">Login Page</h1>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,12 @@
+import { getServerSession } from "next-auth";
 import RegisterForm from "./register-form";
+import { redirect } from "next/navigation";
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  const session = await getServerSession();
+  if (session) {
+    redirect("/");
+  }
   return (
     <div className="mx-auto">
       <h1 className="text-2xl text-center mt-20<">Register Page</h1>
